80a10be366165a412941d0c0633aeb241d12c9b4
sonny <sonlv@templaza.com>
2015-04-09 20:01:35 +0700

    fix error with nav

--- code/assets/js/doc.js
+++ code/assets/js/doc.js
@@ -3,10 +3,24 @@
  */
 
 $(function(){
-    $("#navigator").load("nav.html", function (){
+    var ex_url      = $('meta[name="ex_url"]').attr('content');
+    var nav_url     = (ex_url!=undefined)?ex_url + 'nav.html' : 'nav.html';
+    var footer_url  = (ex_url!=undefined)?ex_url + 'footer.html' : 'footer.html';
+    var regex = /^\#/g;
+
+    $("#navigator").load(nav_url, function (){
         $("li[data-menu='"+$('meta[name="nav"]').attr('content')+"']").addClass('active').delay(5000);
+        if (ex_url!=undefined) {
+            $('#mainnav a').each(function(){
+                if ($(this).attr('href') != '' && !$(this).attr('href').match(regex)) {
+                    $(this).attr('href',ex_url + $(this).attr('href'));
+                }
+            });
+        }
     });
-    $("#doc-footer").load("footer.html");
+    $("#doc-footer").load(footer_url);
+
+
 
 });
 
@@ -25,5 +39,7 @@ $(document).ready(function(){
         }
     });
 });
+
+
 
 
